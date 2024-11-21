f0b1bb5e7380fb45e134ee77d48ec5ce51bfa6d0
farhana988 <farhana744255@gmail.com>
2024-11-21 17:19:23 +0600

    envlocal

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCrZM4YOfU4VDizJcj5yZaWopRUMIltrAo",
-  authDomain: "assi9-2a57f.firebaseapp.com",
-  projectId: "assi9-2a57f",
-  storageBucket: "assi9-2a57f.firebasestorage.app",
-  messagingSenderId: "221505732876",
-  appId: "1:221505732876:web:f67fa410daea0f94b00929"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
